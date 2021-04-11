ec53e55931191e846ef226c2d12525798f872679
fachao0 <43458344+fachao0@users.noreply.github.com>
2021-04-11 22:52:34 +0800

    fachao Couse2

---
+++ 02 元素、分支、循环/code/branch.py
@@ -0,0 +1,35 @@
+x = 5
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print(x,y)
+
+"""
+PHP:
+if(x > 1){
+    y = 3 * x - 5;
+} elseif(x >= -1){
+    y = x + 2;
+} else{
+    y = 5 * x + 3;
+}
+"""
+
+
+# switch/case 使用字典 实现
+def num_to_string(num):
+    numbers = {
+        0 : "zero",
+        1 : "one",
+        2 : "two",
+        3 : "three"
+    }
+
+    return numbers.get(num, None)
+
+if __name__ == "__main__":
+    print(num_to_string(2))
+    print(num_to_string(5))
