1882168f892fb54cc9afc1086b88445d993b31af
zhupeng <17601010540@163.com>
2021-04-13 17:08:12 +0800

    function

--- Exercise/zp/function.py
+++ Exercise/zp/function.py
@@ -43,7 +43,70 @@ def variable_param(*number):
     return '总和是：' + str(sum)
 
 
-print(variable_param(1, 2, 3, 4, 5, 6))
+print(variable_param(1, 2))
 print(variable_param())
-shuzu = [1, 2, 3, 4, 5, 6, 7]  # tuple 元组
+shuzu = (1, 2, 3, 4, 5, 6, 7)  # tuple 元组
 print(variable_param(*shuzu))
+# *shuzu表示把shuzu这个list的所有元素作为可变参数传进去
+# print(variable_param('Bob', 35, city='Beijing'))
+
+
+# 关键字参数说明
+def keyword_param(name, age, **args):
+    print('name:', name, ' age:', str(age), ' other:', args)
+    return
+
+
+keyword_param('Bob', 35)
+keyword_param('Bob', 35, city='Beijing')
+keyword_param('Bob', 35, city='Beijing', area="chaoyang")
+extra = {'city': 'Beijing', 'area': 'chaoyang', 'street': 'jintailu'}
+keyword_param('Bob', 35, **extra)
+# **extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**args参数
+
+
+# 参数组合 默认参数 + 必备参数 + 可变参数 +关键字参数
+def compose(a, b, c=0, *number, **args):
+    print('a =', a, 'b =', b, 'c =', c, 'number =', number, 'args =', args)
+    return
+
+
+number = (4, 5, 6)
+eg     = ('十三', '十四', '十五')
+args   = {'name': '张三', 'age': '28', 'sex': 'woman'}
+compose(1, 2)
+compose(1, 2, 3)
+compose(1, 2, 3, *number)
+compose(1, 2, *number)   # 当默认参数和可变函数一起使用，不传入默认参数且传入可变参数的时候，可变参数的元组前几个参数会填充到默认参数位置上
+# compose(1, 2, *eg)
+compose(1, 2, **args)
+compose(1, 2, 3, *number, **args)
+
+print ('模块')
+# 没有类
+import math
+print(math.ceil(3.45))
+
+from math import ceil
+print(ceil(3.45))
+
+from math import *
+print(ceil(3.45))
+
+# 有类
+import random
+print(random.Random().randint(1, 6))
+print(random.randint(1, 6))
+
+from random import *
+print(randint(1, 6))
+print(Random().randint(1, 6))
+
+print('-----')
+import module3
+print(module3)
+
+
+
+
+
---
+++ Exercise/zp/module3.py
@@ -0,0 +1,6 @@
+
+def foo():
+    return 2
+
+if __name__ == '__main__':
+    print(foo())
